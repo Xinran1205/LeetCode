badfdfc1e0862159e249abaf3417a311374a73f9
Xinran1205 <id19037@bristol.ac.uk>
2025-02-16 12:22:30 +0000

    1

--- 动态规划/零钱兑换2/Solution.java
+++ 动态规划/零钱兑换2/Solution.java
@@ -1,6 +1,11 @@
 // 这个比之前的零钱兑换要简单
 class Solution {
     public int change(int amount, int[] coins) {
+
+        // 这种计算个数的，遍历顺序非常固定！
+        // 如果计算排列（考虑不同顺序）必须先背包后物品
+        // 本体是计算组合（不考虑不同顺序）必须先物品后背包
+
         // dp数组的含义：dp[i]表示凑成总金额为i的组合数
         int[] dp = new int[amount+1];
         dp[0] = 1;
